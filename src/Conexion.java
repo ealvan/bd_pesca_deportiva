0ef0141eca19f3c05724d0c73a75fe4cbab7a72a
edsel <ealvan@unsa.edu.pe>
2021-07-15 14:06:26 -0500

    agregando cambios de jeampier, hizo un menu general y es funcional, creo .., no lo probe

--- src/Conexion.java
+++ src/Conexion.java
@@ -1,5 +1,4 @@
 import java.sql.*;
-import java.util.ArrayList;
 public class Conexion {
 				// Definir la ruta de la base de datos
 				// establenciendo la time zone UTC
@@ -7,7 +6,7 @@ public class Conexion {
 				 // Definir el nombre de usuario de la base de datos
 				private String dbUser = "root";
 				 // Definir la contraseña de la base de datos
-				private String dbPassword = "edsel";
+				private String dbPassword = "Jeampier123A";
 				 // Definir controlador de carga
 				private String jdbcName = "com.mysql.cj.jdbc.Driver";
 			 
@@ -16,11 +15,13 @@ public class Conexion {
 					Connection conn = null;
 					try {
 						Class.forName(jdbcName);
+						System.out.println("Se cargo el controlador");
 					} catch (Exception e) {
 						 System.out.println ("¡Error al cargar el controlador!");
 					}
 					try {
 						conn = (Connection) DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+						System.out.println("Se pudo conectar a la base de datos");
 					} catch (SQLException ex) {
 						 ex.printStackTrace();
 						 System.out.println ("¡Error al conectarse a la base de datos!");
@@ -32,7 +33,10 @@ public class Conexion {
 				public static void main(String[] args) {
 					Conexion a = new Conexion();
 					Connection con = a.getConn();
-					getData("eventos",con);
+					//display(con);
+					//insert(con);
+					//update(con);
+					
 				}
 				public Object[][] retrieveDataFromSelect(String table, Connection con, int cols) {
 					String query = "SELECT * FROM " + table;
@@ -126,92 +130,8 @@ public class Conexion {
 						e.printStackTrace();
 					}
 				}
-				static ArrayList<String> getColumns(String tableName, Connection con) {
-					if(tableName.isEmpty()) {
-						tableName="peces";
-					}
-					ArrayList<String> columns = new ArrayList<String>();
-					
-					String query = "SHOW COLUMNS FROM " + tableName+";";
-					
-					try {
-						Statement stmt = con.createStatement();  
-						ResultSet rs = stmt.executeQuery(query);
-						while(rs.next()) {
-							columns.add(rs.getString(1));
-						}
-						//System.out.println(columns.toString());
-						
-					}catch(Exception e) {
-						e.printStackTrace();
-						return null;
-					}
-					return columns;
-				}
-				//get data, nos devuelve los datos, junto con sus columnas
-				//de una tabla que se especifique en tableName
-				static ArrayList<String[]> getData(String tableName, Connection con) {
-					//obtener columnas
-					ArrayList<String> columns = getColumns(tableName, con);
-					ArrayList<String[]> data = new ArrayList<String[]>();
-					//pasamos a un array statico las columnas
-					String[] cols = new String[columns.size()];
-					int p = 0;
-					for(String col: columns) {
-						cols[p] = col;
-						p++;
-					}
-					//agregamos como el head de la tabla
-					//siempre la primera fila, son las columnas!
-					data.add(cols);
-					
-					//get type of data
-					String[] types = new String[columns.size()];
-					//son las queryes que usamos
-					//qtypes, se usa para la descripcion de de la tabla
-					String qTypes = "SHOW COLUMNS FROM " + tableName+";";
-					String qData = "SELECT * FROM "+tableName+";";
-					try {
-						Statement stmt = con.createStatement();
-						ResultSet rs = stmt.executeQuery(qTypes);
-						int i =0;
-						while(rs.next()) {
-							types[i] = rs.getString(2);
-							i++;
-						}
-						
-						//get table of data
-						Statement stmtData = con.createStatement();
-						ResultSet resSet = stmtData.executeQuery(qData);
-						//vamos fila, por fila
-						while(resSet.next()) {
-							String[] row = new String[types.length];
-							//ahora vamos por las celdas de la fila
-							for(int j =1; j <= row.length; j++) {
-								if(types[j-1].startsWith("int")) {
-									row[j-1] = String.valueOf(resSet.getInt(j));
-								}else {
-									row[j-1] = resSet.getString(j);
-								}
-							}
-							data.add(row);
-						}
-						/*
-						 * for(int q =0; q < data.size(); q++) {
-							for(int u =0; u < data.get(q).length; u++) {
-								System.out.print(data.get(q)[u] + " " );
-							}
-							System.out.println();
-						}
-						 * 
-						 * */
-						
-					}catch(Exception e ) {
-						e.printStackTrace();
-					}
-					//retornamos, si no se logra, tendra un error
-					//de indexOfBounds Exception o otro error del ArrayList
-					return data;
-				}
+			
+				
+				
 				
 }
